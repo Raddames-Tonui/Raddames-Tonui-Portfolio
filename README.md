9910cd6389452e4f0a9b4be8975f3138e91b51a8
Raddames <raddamestonui48@gmail.com>
2024-03-17 23:22:22 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+ PERSONAL PORTFOLIO
+
+ INSTALLATION
+ Run in terminal: 
+
+ npx tailwindcss -i ./input.css -o ./output.css --watch
+
+ *To use tailwind.custom.config.js*
+ Run in terminal: 
+
+npm run build-css
+
---
+++ tailwind.custom.config.js
@@ -0,0 +1,29 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors:{
+      'hulk':{
+        light : '#10CA02',
+        default : '#01A003',
+        dark : '#006D05',
+      },
+      'gray':{
+        light : '#C5C5C5',
+        default : '#747172',
+        dark : '#5A5A5A',
+      },
+      'orange':{
+        light: '#FC621C',
+      },
+      'navy':{
+        light: '#131B2A',
+        default : '#0D1219',
+        dark: '#050912 ',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
