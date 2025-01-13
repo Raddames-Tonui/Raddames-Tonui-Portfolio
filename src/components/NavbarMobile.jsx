715625bddd4c914d022e4fbe01da827dae30999b
Raddames Tonui <raddamestonui48@gmail.com>
2025-01-13 12:45:06 +0300

    added navbar

--- src/components/NavbarMobile.jsx
+++ src/components/NavbarMobile.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function NavbarMobile({ showNavbar, setShowNavbar, navbarRef }) {
+function NavbarMobile({ showNavbar, navbarRef }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
@@ -16,7 +16,9 @@ function NavbarMobile({ showNavbar, setShowNavbar, navbarRef }) {
   return (
     <nav
       ref={navbarRef}
-      className={`bg-gray-900 text-lime-500 transition-transform duration-300 ${showNavbar ? "translate-y-0" : "-translate-y-full"} fixed top-0 left-0 w-full px-10 z-50 py-5`}
+      className={`text-lime-500 transition-transform duration-300 ${
+        showNavbar ? "translate-y-0" : "-translate-y-full"
+      } fixed top-0 left-0 w-full px-10 z-50 py-5`}
     >
       <div className="flex justify-end items-center">
         {/* Hamburger Button for Small Screens */}
@@ -28,7 +30,7 @@ function NavbarMobile({ showNavbar, setShowNavbar, navbarRef }) {
             // X icon when menu is open
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="w-8 h-8 "
+              className="w-8 h-8"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -63,50 +65,70 @@ function NavbarMobile({ showNavbar, setShowNavbar, navbarRef }) {
         <div
           className={`${
             isMenuOpen
-              ? "fixed top-16  right-0 min-h-screen w-3/4 bg-gray-900 text-lime-500 flex flex-col items-center justify-evenly gap-6 p-8 transition-all duration-300"
+              ? "fixed top-0 right-0 min-h-[120vh] w-3/4 bg-gray-900 text-lime-500 p-8 transition-all duration-300"
               : "hidden"
           } lg:hidden text-lg font-medium`}
         >
-          <a
-            href="#home"
-            onClick={(e) => {
-              e.preventDefault();
-              handleNavClick("home");
-            }}
-            className="hover:text-lime-600 hover:underline border-b border-lime-500 pb-4"
+          {/* Close Icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-8 h-8 right-6 absolute top-6 "
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            onClick={toggleMenu}
           >
-            Home
-          </a>
-          <a
-            href="#about"
-            onClick={(e) => {
-              e.preventDefault();
-              handleNavClick("about");
-            }}
-            className="hover:text-lime-600 hover:underline border-b border-lime-500 pb-4"
-          >
-            About
-          </a>
-          <a
-            href="#projects"
-            onClick={(e) => {
-              e.preventDefault();
-              handleNavClick("projects");
-            }}
-            className="hover:text-lime-600 hover:underline border-b border-lime-500 pb-4"
-          >
-            Projects
-          </a>
-          <a
-            href="#contact"
-            onClick={(e) => {
-              e.preventDefault();
-              handleNavClick("contact");
-            }}
-            className="hover:text-lime-600 hover:underline pb-4"
-          >
-            Contact
-          </a>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+
+          {/* Navigation Links */}
+          <div className="flex flex-col h-[70vh] justify-evenly mt-5">
+            <a
+              href="#home"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNavClick("home");
+              }}
+              className="hover:text-lime-600 hover:underline border-b border-lime-500 pb-4"
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNavClick("about");
+              }}
+              className="hover:text-lime-600 hover:underline border-b border-lime-500 pb-4"
+            >
+              About
+            </a>
+            <a
+              href="#projects"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNavClick("projects");
+              }}
+              className="hover:text-lime-600 hover:underline border-b border-lime-500 pb-4"
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              onClick={(e) => {
+                e.preventDefault();
+                handleNavClick("contact");
+              }}
+              className="hover:text-lime-600 hover:underline border-b border-lime-500 pb-4"
+            >
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </nav>
